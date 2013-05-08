3f959f9c3c5e2deddc8fd9081114c2d797fbf5cf
Sven <sven@dev-vm>
2013-05-08 18:26:00 +0200

    added comment

--- bridge.js
+++ bridge.js
@@ -5,6 +5,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    // what about X-Requested-With, X-HTTP-Method-Override, Accept ?
 
     if (req.method === 'OPTIONS') {
         res.end();
